fb14529f8747daa4b0cc638dfcd0aef3926fd42b
Gabriel Thiego <gabrielthiego@gmail.com>
2025-07-07 12:25:36 -0300

    teste 1

--- cypress/e2e/UI/testes-web/carrinho.cy.js
+++ cypress/e2e/UI/testes-web/carrinho.cy.js
@@ -1,66 +1,62 @@
 function selecionarVariacoes() {
-  cy.get('.button-variable-item-XL').then(($el) => {
+  cy.get('.button-variable-item-XL', { timeout: 10000 }).then(($el) => {
     if ($el.length && !$el.hasClass('disabled')) {
       cy.wrap($el).click({ force: true });
     } else {
-      cy.get('[class^="button-variable-item"]').not('.disabled').first().click({ force: true });
+      cy.get('[class^="button-variable-item"]', { timeout: 10000 }).not('.disabled').first().click({ force: true });
     }
   });
 
-  cy.get('.button-variable-item-Green').then(($el) => {
+  cy.get('.button-variable-item-Green', { timeout: 10000 }).then(($el) => {
     if ($el.length && !$el.hasClass('disabled')) {
       cy.wrap($el).click({ force: true });
     } else {
-      cy.get('[class^="button-variable-item"]').not('.disabled').first().click({ force: true });
+      cy.get('[class^="button-variable-item"]', { timeout: 10000 }).not('.disabled').first().click({ force: true });
     }
   });
 }
 
 describe('[US-0001] Adicionar item ao carrinho', () => {
   beforeEach(() => {
-    cy.visit('http://localhost/product/abominable-hoodie/');
+    cy.visit('http://localhost/product/abominable-hoodie/', { timeout: 10000 });
   });
 
   it('Adicionar item ao carrinho com tamanho XL e cor Green (ou próximo disponível)', () => {
     selecionarVariacoes();
 
-    cy.get('.single_add_to_cart_button').click({ force: true });
+    cy.get('.single_add_to_cart_button', { timeout: 10000 }).click({ force: true });
     selecionarVariacoes();
-    cy.get('.single_add_to_cart_button').click({ force: true });
+    cy.get('.single_add_to_cart_button', { timeout: 10000 }).click({ force: true });
 
-    cy.get('.woocommerce-message').should('contain', 'foi adicionado no seu carrinho');
-    cy.get('.woocommerce-message a').contains('Ver carrinho').click({ force: true });
+    cy.get('.woocommerce-message', { timeout: 10000 }).should('contain', 'foi adicionado no seu carrinho');
+    cy.get('.woocommerce-message a', { timeout: 10000 }).contains('Ver carrinho').click({ force: true });
   });
 
   it('Adicionar dois itens iguais ao carrinho', () => {
     selecionarVariacoes();
 
-    cy.get('.single_add_to_cart_button').click({ force: true });
+    cy.get('.single_add_to_cart_button', { timeout: 10000 }).click({ force: true });
     selecionarVariacoes();
-    cy.get('.single_add_to_cart_button').click({ force: true });
-       cy.get('.plus').click({ force: true });
+    cy.get('.single_add_to_cart_button', { timeout: 10000 }).click({ force: true });
 
-    cy.get('.woocommerce-message').should('contain', 'foi adicionado no seu carrinho');
-    cy.get('.woocommerce-message a').contains('Ver carrinho').click({ force: true });
-    
-   
+    cy.get('.plus', { timeout: 10000 }).click({ force: true });
+
+    cy.get('.woocommerce-message', { timeout: 10000 }).should('contain', 'foi adicionado no seu carrinho');
+    cy.get('.woocommerce-message a', { timeout: 10000 }).contains('Ver carrinho').click({ force: true });
   });
 
   it('Adicionar e remover item do carrinho', () => {
-
-   selecionarVariacoes();
-
-    cy.get('.single_add_to_cart_button').click({ force: true });
     selecionarVariacoes();
-    cy.get('.single_add_to_cart_button').click({ force: true });
 
-    cy.get('.woocommerce-message').should('contain', 'foi adicionado no seu carrinho');
-    cy.get('.woocommerce-message a').contains('Ver carrinho').click({ force: true });
-    // Clica no botão de remover item do carrinho
-    cy.get('.remove > .fa').click({ force: true });
+    cy.get('.single_add_to_cart_button', { timeout: 10000 }).click({ force: true });
+    selecionarVariacoes();
+    cy.get('.single_add_to_cart_button', { timeout: 10000 }).click({ force: true });
 
-// Confirma que o carrinho está vazio
-    cy.contains('Seu carrinho está vazio.').should('be.visible');
+    cy.get('.woocommerce-message', { timeout: 10000 }).should('contain', 'foi adicionado no seu carrinho');
+    cy.get('.woocommerce-message a', { timeout: 10000 }).contains('Ver carrinho').click({ force: true });
 
+    cy.get('.remove > .fa', { timeout: 10000 }).click({ force: true });
+
+    cy.contains('Seu carrinho está vazio.', { timeout: 10000 }).should('be.visible');
   });
 });
