e384391d9442cefa8edd806b6e47ec1597fde478
Gabriel Thiego <gabrielthiego@gmail.com>
2025-07-08 11:05:13 -0300

    test 4

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
  e2e: {
-    defaultCommandTimeout: 10000,
-    pageLoadTimeout: 20000,
+    defaultCommandTimeout: 50000,
+    pageLoadTimeout: 100000,
     baseUrl: 'http://localhost:80',
     specPattern: 'cypress/e2e/UI/testes-web/**/*.cy.js',
     supportFile: 'cypress/support/e2e.js',
